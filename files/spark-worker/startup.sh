52d08d0f29408ba9a5534290d2091294f6d154d0
Vasily Vasinov <des.elyon@gmail.com>
2020-10-16 16:06:23 -0600

    Update worker startup script

--- files/spark-worker/startup.sh
+++ files/spark-worker/startup.sh
@@ -3,7 +3,7 @@
 set -e
 
 /usr/local/spark/sbin/start-slave.sh \
+  {{ engines.spark-master.vars.url }} \
   -p {{ engines.spark-worker.vars.port }} \
   --webui-port {{ engines.spark-worker.vars.dashboard_port }} \
-  --properties-file {{ engines.spark-worker.orchestrator.container.mounts.engine }}/spark.conf \
-  {{ engines.spark-master.vars.url }}
+  --properties-file {{ engines.spark-worker.orchestrator.container.mounts.engine }}/spark.conf
