147cbe58ee98b424fbf7c927998d799f26316a3d
Vasily Vasinov <des.elyon@gmail.com>
2020-10-16 14:22:39 -0600

    Updating app.

--- files/spark/worker_startup.sh
+++ files/spark/worker_startup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-start.sh /usr/local/spark/sbin/start-master.sh \
+/usr/local/spark/sbin/start-master.sh \
   -p {{ engines.spark.vars.spark_worker_port }} \
   --webui-port {{ engines.spark.vars.spark_worker_ui_port }} \
   spark://{{ engines.spark.vars.spark_master_url }}
