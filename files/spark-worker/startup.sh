831ae90640dd16a2e2c476ef6c7f9d46105e18a1
Vasily Vasinov <des.elyon@gmail.com>
2020-10-16 15:13:39 -0600

    Update config file with CPUs and memory

--- files/spark-worker/startup.sh
+++ files/spark-worker/startup.sh
@@ -5,4 +5,4 @@ set -e
 /usr/local/spark/sbin/start-slave.sh {{ engines.spark-master.vars.url }} \
   -p {{ engines.spark-worker.vars.port }} \
   --webui-port {{ engines.spark-worker.vars.dashboard_port }}
-  --properties-file {{ engines.spark-master.orchestrator.container.mounts.engine }}/spark.conf
+  --properties-file {{ engines.spark-worker.orchestrator.container.mounts.engine }}/spark.conf
