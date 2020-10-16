545967949fb84a0799d6075e79ab898421f4a8e8
Vasily Vasinov <des.elyon@gmail.com>
2020-10-16 15:16:18 -0600

    Fix Spark conf files

--- files/spark-worker/startup.sh
+++ files/spark-worker/startup.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-/usr/local/spark/sbin/start-slave.sh {{ engines.spark-master.vars.url }} \
+/usr/local/spark/sbin/start-slave.sh \
   -p {{ engines.spark-worker.vars.port }} \
-  --webui-port {{ engines.spark-worker.vars.dashboard_port }}
-  --properties-file {{ engines.spark-worker.orchestrator.container.mounts.engine }}/spark.conf
+  --webui-port {{ engines.spark-worker.vars.dashboard_port }} \
+  --properties-file {{ engines.spark-worker.orchestrator.container.mounts.engine }}/spark.conf \
+  {{ engines.spark-master.vars.url }}
