00fc18d5a2a28adcdd6d332f3cd09cd37d6483b5
Vasily Vasinov <des.elyon@gmail.com>
2020-11-12 10:46:32 -0700

    Update YAML

--- files/spark_master/startup.sh
+++ files/spark_master/startup.sh
@@ -6,4 +6,4 @@ set -e
   -h {{ engines.spark_master.vars.private_ip }} \
   -p {{ engines.spark_master.networking.ports.master.value }} \
   --webui-port {{ engines.spark_master.networking.ports.dashboard.value }} \
-  --properties-file {{ engines.spark_master.orchestrator.container.mounts.app_files.spark_master.container_path }}/spark.conf
+  --properties-file {{ engines.spark_master.container.mounts.app_files.spark_master.container_path }}/spark.conf
--- files/spark_worker/startup.sh
+++ files/spark_worker/startup.sh
@@ -5,5 +5,5 @@ set -e
 /usr/local/spark/sbin/start-slave.sh \
   -p {{ engines.spark_worker.networking.ports.worker.value }} \
   --webui-port {{ engines.spark_worker.networking.ports.dashboard.value }} \
-  --properties-file {{ engines.spark_worker.orchestrator.container.mounts.app_files.spark_worker.container_path }}/spark.conf \
+  --properties-file {{ engines.spark_worker.container.mounts.app_files.spark_worker.container_path }}/spark.conf \
   {{ engines.spark_master.vars.url }}
